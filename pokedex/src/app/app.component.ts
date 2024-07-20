316b465ff48cf005a16a3b6a04fdd1f0ca937036
konradparypa <konrad.parypa@gmail.com>
2024-07-20 14:19:14 +0200

    fix import paths

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {PokemonService} from "./core/services/pokemon.service";
-import {HeaderComponent} from "./core/components/header/header.component";
-import {FooterComponent} from "./core/components/footer/footer.component";
+import {RouterOutlet} from '@angular/router';
+import {PokemonService} from "@core/services/pokemon.service";
+import {HeaderComponent} from "@core/components/header/header.component";
+import {FooterComponent} from "@core/components/footer/footer.component";
 import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
--- pokedex/src/app/app.routes.ts
+++ pokedex/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import {PageNotFoundComponent} from "./core/components/page-not-found/page-not-found.component";
-import {PokemonListComponent} from "./features/pokemon-list/pokemon-list.component";
+import {PageNotFoundComponent} from "@core/components/page-not-found/page-not-found.component";
+import {PokemonListComponent} from "@features/pokemon-list/pokemon-list.component";
 
 export const routes: Routes = [
   {path: 'pokemon-list', component: PokemonListComponent},
