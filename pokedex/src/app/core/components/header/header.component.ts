66517902c3cc71f247a7642ff3511c69072d0952
konradparypa <konrad.parypa@gmail.com>
2024-07-20 16:51:13 +0200

    add header and footer basic styling

--- pokedex/src/app/core/components/header/header.component.ts
+++ pokedex/src/app/core/components/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardImage} from "@angular/material/card";
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatCardImage, NgOptimizedImage, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
