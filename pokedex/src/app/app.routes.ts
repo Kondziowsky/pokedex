bda8839778a2b70cd4a8201020ea9ba7e210d3f1
konradparypa <konrad.parypa@gmail.com>
2024-07-22 23:16:19 +0200

    add lazy loading standalone approach

--- pokedex/src/app/app.routes.ts
+++ pokedex/src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import {Routes} from '@angular/router';
-import {PageNotFoundComponent} from "@core/components/page-not-found/page-not-found.component";
-import {PokemonListComponent} from "@features/pokemon-list/pokemon-list.component";
-import {PokemonDetailsComponent} from "@features/pokemon-details/pokemon-details.component";
 
 export const routes: Routes = [
-  { path: 'pokemon-list', component: PokemonListComponent },
-  { path: 'pokemon-details/:id', component: PokemonDetailsComponent },
+  { path: 'pokemon-list', loadComponent: () => import("./features/pokemon-list/pokemon-list.component").then((m) => m.PokemonListComponent) },
+  { path: 'pokemon-details/:id', loadComponent: () => import("./features/pokemon-details/pokemon-details.component").then((m) => m.PokemonDetailsComponent) },
   { path: '', redirectTo: '/pokemon-list', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', loadComponent: () => import("./core/components/page-not-found/page-not-found.component").then((m) => m.PageNotFoundComponent) }
 ];
